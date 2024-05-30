42713818c67e18b5f53455f4b6128e0b2f27c39f
Aa-sheesh <aashish4912345@gmail.com>
2024-05-31 00:03:09 +0530

    failed, understood the concept tho

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AddTodo />
+      <Todos />
     </>
   )
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function Todos() {
+    const todos= useSelector((state)=>{state.todos})
+  return (
+    <>
+      <div>Todos</div>
+      {todos.map((todo) => (
+        <div key={todo.id}>{todo.text}</div>
+      ))}
+    </>
+  )
+}
+
+export default Todos
